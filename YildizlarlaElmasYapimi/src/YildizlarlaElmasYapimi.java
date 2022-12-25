9ac580832db46bdc6c993e983a1f6726bfb8a09f
Mert sezgin <sezgincanmert@gmail.com>
2022-12-25 16:19:14 +0300

    yildizlarla elmas yapımı

---
+++ YildizlarlaElmasYapimi/src/YildizlarlaElmasYapimi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class YildizlarlaElmasYapimi {
+	
+		public static void main(String[] args) {
+			Scanner input=new Scanner(System.in);	
+			
+			System.out.println("sayý giriniz: ");
+			int a=input.nextInt();
+			
+			for(int i=1;i<=a;i++) {
+				for(int k=1;k<=(a-i);k++) {
+				System.out.print(" ");
+			}
+				for(int j=1;j<=(2*i)-1;j++) {
+					System.out.print("*");
+				}
+				 System.out.println();
+		}
+			for(int i=1;i<a;i++) {
+				for(int k=1;k<=(a-i);k++) {
+					System.out.print("n");
+				}
+			}
+			
+	}
+
+}
